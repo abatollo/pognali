7bc521537b5932158e3a44d3513c91ad2f08210e
Aleksandr Batollo <a@batollo.ru>
2020-05-03 05:15:48 +0300

    Добавлены переменные

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,9 @@
 let menuButton = document.querySelector(`.page-header__toggle`);
 let mainMenu = document.querySelector(`.main-nav`);
 
+mainMenu.classList.add('main-nav--hidden');
+
 menuButton.addEventListener(`click`, function (evt) {
     evt.preventDefault;
-    mainMenu.classList.toggle(`display-none`);
+    mainMenu.classList.add('main-nav--hidden');
 })
