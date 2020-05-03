0d0b290f3e9520a3c70f7c05c0e3582111ebe3fa
Aleksandr Batollo <a@batollo.ru>
2020-05-03 09:08:05 +0300

    Исправления шрифта и цвета в hero-section

--- source/js/app.js
+++ source/js/app.js
@@ -1,9 +1,13 @@
 let menuButton = document.querySelector(`.page-header__toggle`);
 let mainMenu = document.querySelector(`.main-nav`);
 
-mainMenu.classList.add('main-nav--hidden');
+mainMenu.classList.add("main-nav--hidden");
 
 menuButton.addEventListener(`click`, function (evt) {
-    evt.preventDefault;
-    mainMenu.classList.toggle('main-nav--hidden');
-})
+  evt.preventDefault;
+  if (mainMenu.classList.contains(`main-nav--hidden`)) {
+    mainMenu.classList.remove(`main-nav--hidden`);
+  } else {
+    mainMenu.classList.add(`main-nav--hidden`);
+  }
+});
